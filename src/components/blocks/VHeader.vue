45a66a37c19303105716c4f56b9671c609481c40
kadonomaro <denis.afer@gmail.com>
2020-08-25 21:00:08 +0700

    feat: add logo component

--- src/components/blocks/VHeader.vue
+++ src/components/blocks/VHeader.vue
@@ -1,16 +1,25 @@
 <template>
-	<header class="header">header</header>
+	<header class="header">
+		<div class="header__logo">
+			<v-logo />
+		</div>
+	</header>
 </template>
 
 <script>
+import VLogo from './VLogo';
+
 export default {
-	name: 'VHeader'
+	name: 'VHeader',
+	components: {
+		VLogo
+	}
 }
 </script>
 
 <style lang="scss">
 	.header {
-		padding: 15px 10px;
+		padding: 10px;
 		background-color: $color-background;
 		border-bottom: 1px solid $color-border;
 		box-shadow: 0 5px 5px rgba($color: #000000, $alpha: 0.05);
---
+++ src/components/blocks/VLogo.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="logo">
+		<img class="logo__image" :src="logo" alt="airganizer logo" width="80">
+		<div class="logo__text">
+			<span class="logo__title">Airganizer</span>
+			<span class="logo__subtitle">Управление личным временем</span>
+		</div>
+	</div>
+</template>
+
+<script>
+import logo from '@/assets/logo.png'
+
+export default {
+	name: 'VLogo',
+	data() {
+		return {
+			logo
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+	.logo {
+		display: flex;
+		align-items: center;
+		&__text {
+			margin-left: 10px;
+			line-height: 1;
+		}
+		&__title {
+			display: block;
+			font-weight: bold;
+		}
+		&__subtitle {
+			font-size: 12px;
+		}
+	}
+</style>
