1e1fdcb67359a1572a1e3e6af8639c8495d8c6f6
kadonomaro <denis.afer@gmail.com>
2020-08-26 00:40:07 +0700

    feat: add navigation component

--- src/components/blocks/VHeader.vue
+++ src/components/blocks/VHeader.vue
@@ -3,25 +3,35 @@
 		<div class="header__logo">
 			<v-logo />
 		</div>
+		<div class="header__nav">
+			<v-navigation />
+		</div>
 	</header>
 </template>
 
 <script>
 import VLogo from './VLogo';
+import VNavigation from './VNavigation';
 
 export default {
 	name: 'VHeader',
 	components: {
-		VLogo
+		VLogo,
+		VNavigation
 	}
 }
 </script>
 
 <style lang="scss">
 	.header {
-		padding: 10px;
+		display: flex;
+		align-items: center;
+		padding: 10px 20px;
 		background-color: $color-background;
 		border-bottom: 1px solid $color-border;
 		box-shadow: 0 5px 5px rgba($color: #000000, $alpha: 0.05);
+		&__logo {
+			margin-right: auto;
+		}
 	}
 </style>
---
+++ src/components/blocks/VNavigation.vue
@@ -0,0 +1,53 @@
+<template>
+	<nav class="navigation">
+		<ul class="navigation__list">
+			<li class="navigation__item">
+				<router-link
+					class="navigation__link navigation__link--login"
+					:to="'login'"
+				>Войти</router-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+
+export default {
+	name: 'VNavigation'
+}
+</script>
+
+<style lang="scss">
+	.navigation {
+		&__list {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+		&__item {
+			display: flex;
+			align-items: center;
+		}
+		&__link {
+			display: block;
+			color: inherit;
+			text-decoration: none;
+		}
+		&__link--login {
+			position: relative;
+			padding-left: 30px;
+			&::before {
+				content: '';
+				position: absolute;
+				top: 50%;
+				left: 0;
+				width: 25px;
+				height: 25px;
+				background-image: url('~@/assets/icons/login.svg');
+				background-repeat: no-repeat;
+				transform: translateY(-50%);
+			}
+		}
+	}
+</style>
