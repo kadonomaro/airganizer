febfe3b032bb5ea924f50bbaaa5b0ac4f9ce7bc7
kadonomaro <denis.afer@gmail.com>
2020-09-15 20:57:02 +0700

    prod: add production firebase config

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ moment.updateLocale('ru', {
 	}
 });
 
-const firebaseConfig = {
+const firebaseDevConfig = {
 	apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
 	authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
 	databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
@@ -29,8 +29,18 @@ const firebaseConfig = {
 	appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
 
+const firebaseProdConfig = {
+	apiKey: 'AIzaSyDo_a33atZd7WdJNW7pDZwr5gnfYWk6iss',
+	authDomain: 'airganizer-app.firebaseapp.com',
+	databaseURL: 'https://airganizer-app.firebaseio.com',
+	projectId: 'airganizer-app',
+	storageBucket: 'airganizer-app.appspot.com',
+	messagingSenderId: '946242446859"',
+	appId: '1:946242446859:web:ee45eee0740c7417abc90e'
+};
 
-firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(process.env.NODE_ENV === 'production' ? firebaseProdConfig : firebaseDevConfig);
 const auth = firebase.auth();
 const database = firebase.database();
 export { auth, database };
