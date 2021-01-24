4f46430eb53dbc3762d51f44be72bbe9ffbe69ba
kadonomaro <denis.afer@gmail.com>
2021-01-24 16:41:42 +0700

    feat: add expand for TaskListDay component

--- src/components/TaskList/TaskListComponent.vue
+++ src/components/TaskList/TaskListComponent.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="task-list">
+		<span class="task-list__title" @click="toggleAllExpand">{{ computeTitle }}</span>
 		<div class="task-list__day" v-for="(day, key) in getDateByAllDays" :key="key">
-			<task-list-day :day="day" :title="key" />
+			<task-list-day :day="day" :title="key" :all-expanded="isAllExpanded" />
 		</div>
 	</div>
 </template>
@@ -15,8 +16,25 @@ export default {
 	components: {
 		TaskListDay
 	},
+	data() {
+		return {
+			isAllExpanded: true
+		}
+	},
 	computed: {
-		...mapGetters(['getDateByAllDays'])
+		...mapGetters(['getDateByAllDays']),
+		computeTitle() {
+			if (this.isAllExpanded) {
+				return 'Скрыть все';
+			} else {
+			return 'Показать все'
+			}
+		}
+	},
+	methods: {
+		toggleAllExpand() {
+			this.isAllExpanded = !this.isAllExpanded;
+		}
 	}
 }
 </script>
@@ -24,9 +42,14 @@ export default {
 <style lang="scss">
 .task-list {
 	padding: 30px 0;
-
+	&__title {
+		display: inline-block;
+		margin-bottom: 10px;
+		font-size: 18px;
+		cursor: pointer;
+	}
 	&__day {
-		margin-bottom: 15px;
+		margin-bottom: 5px;
 	}
 }
 </style>
--- src/components/TaskList/TaskListDay.vue
+++ src/components/TaskList/TaskListDay.vue
@@ -1,7 +1,12 @@
 <template>
 	<div class="task-list-day">
-		<span class="task-list-day__title" v-if="day.data.length">{{ title }}</span>
-		<ul class="task-list-day__list" v-if="day.data.length">
+		<span
+			class="task-list-day__title"
+			:class="{ 'task-list-day__title--opened': isExpanded }"
+			v-if="day.data.length"
+			@click="toggleExpand"
+		>{{ title }}</span>
+		<ul class="task-list-day__list" v-if="day.data.length" v-show="isExpanded">
 			<li class="task-list-day__item" v-for="task in day.data" :key="task.id">
 				<calendar-tasks-item
 					:day="{ value: title, editable: true }"
@@ -23,6 +28,10 @@ export default {
 		CalendarTasksItem
 	},
 	props: {
+		allExpanded: {
+			type: Boolean,
+			required: true
+		},
 		title: {
 			type: String,
 			required: true
@@ -32,10 +41,25 @@ export default {
 			required: true
 		}
 	},
+	data() {
+		return {
+			isExpanded: true
+		}
+	},
 	computed: {
 		...mapState({
 			settings: state => state.settings.settings
 		})
+	},
+	methods: {
+		toggleExpand() {
+			this.isExpanded = !this.isExpanded;
+		}
+	},
+	watch: {
+		allExpanded(value) {
+			this.isExpanded = value;
+		}
 	}
 }
 </script>
@@ -43,9 +67,29 @@ export default {
 <style lang="scss">
 .task-list-day {
 	&__title {
-		display: block;
-		margin-bottom: 5px;
+		position: relative;
+		display: inline-block;
+		padding: 6px 3px;
 		font-weight: bold;
+		cursor: pointer;
+		transition: color 0.1s ease-in;
+		&::before {
+			content: '';
+			position: absolute;
+			top: 12.5px;
+			right: -10px;
+			border-style: solid;
+			border-width: 5px 0 5px 7px;
+			border-color: transparent transparent transparent $color-text;
+			transition: transform 0.1s ease-in, border-color 0.1s ease-in;
+		}
+	}
+	&__title--opened {
+		color: $color-brand;
+		&::before {
+			border-color: transparent transparent transparent $color-brand;
+			transform: rotate(90deg);
+		}
 	}
 
 	&__list {
